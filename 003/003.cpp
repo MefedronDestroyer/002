32ea7b8949bca8628f7e7c2864f9077705bc17cb
MefedronDestroyer <151124942+MefedronDestroyer@users.noreply.github.com>
2023-11-20 13:01:13 +0300

    Add files via upload

---
+++ 003/003.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int mas[10];
+	mas[0] = 0;
+	mas[1] = 1;
+	mas[2] = 2;
+	mas[3] = 3;
+	mas[4] = 4;
+	mas[5] = 5;
+	mas[6] = 6;
+	mas[7] = 7;
+	mas[8] = 8;
+	mas[9] = 9;
+	int n = 0;
+	for (int q = 0; q <= 10; q++) {
+		if (q % 2 != 0) {
+			n += q;
+		}
+	}
+	cout << "Nechetnih chisel: " << n << endl;
+}
